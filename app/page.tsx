00adab6d8a8cc05790987e2297c1257b5a3cc987
Eugene_Korzh <korzh367@gmail.com>
2025-12-24 20:53:07 +0100

    fix main page

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,13 @@ export default async function Home() {
   const res = await fetchPopCoins(1);
   return (
     <div className="flex flex-col items-center px-6 py-10 text-white min-h-screen">
-      {/* Інформація про сайт */}
       <div className="text-center mb-10 max-w-2xl">
         <h1 className="text-4xl font-bold text-blue-400">💰 Crypto Tracker</h1>
         <p className="text-gray-400 mt-3">
-          Track, explore, and stay updated — real-time cryptocurrency prices at
+          Track, explore and stay updated — real-time cryptocurrency prices at
           your fingertips
         </p>
       </div>
-      {/* Популярні монети */}
       <div className="w-full max-w-4xl">
         <h2 className="text-2xl font-semibold mb-4 text-center">
           Популярні монети
